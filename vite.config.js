f9c975926683e3a1643d450de02dd9ef07a1378e
Nicholas Gillespie <contact@nicholasgillespie.com>
2024-03-18 13:35:10 +0100

    Add rollupOptions to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import vitePurgeCss from "vite-plugin-purgecss";
 
 export default defineConfig({
   plugins: [vitePurgeCss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
