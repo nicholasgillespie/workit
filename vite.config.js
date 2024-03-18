7803f2052d957752a0e5cafb8481cffc59151253
Nicholas Gillespie <contact@nicholasgillespie.com>
2024-03-18 14:28:05 +0100

    Update Vite config to set output directory and empty it before building

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import { resolve } from "path";
 
 import vitePurgeCss from "vite-plugin-purgecss";
 
-console.log(__dirname);
-console.log(resolve(__dirname, "index.html"));
-console.log(resolve(__dirname, "src/"));
-
 export default defineConfig({
   plugins: [vitePurgeCss()],
   root: resolve(__dirname, "src/"),
   build: {
+    outDir: "../dist",
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         home: resolve(__dirname, "src/index.html"),
