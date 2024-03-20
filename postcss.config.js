110a7ec88df224b21f9ed495fa8e1a7229e62e26
Nicholas Gillespie <contact@nicholasgillespie.com>
2024-03-20 18:00:33 +0100

    Testing 100vh / dvh.

--- postcss.config.js
+++ postcss.config.js
@@ -9,22 +9,22 @@ export default {
       // Stage 3: Candidate recommendation stage, features await final review.
       // Stage 4: Final stage, features are official CSS features.
       // Features at stage level or higher will be polyfilled.
-      stage: 4,
+      stage: 3,
       features: {
         // Add or remove specific features here.
         // If feature is set to true ? polyfilled : not be polyfilled.
         // List of features & stages: https://preset-env.cssdb.org/features/
-        // "opacity-percentage": true,
-        // "text-decoration-shorthand": true,
-        // "logical-properties-and-values": false,
-        // "prefers-color-scheme-query": false,
-        // "gap-properties": false,
-        // "custom-properties": false,
-        // "place-properties": false,
-        // "not-pseudo-class": false,
-        // "focus-visible-pseudo-class": false,
-        // "focus-within-pseudo-class": false,
-        // "color-functional-notation": false,
+        "opacity-percentage": true,
+        "text-decoration-shorthand": true,
+        "logical-properties-and-values": false,
+        "prefers-color-scheme-query": false,
+        "gap-properties": false,
+        "custom-properties": false,
+        "place-properties": false,
+        "not-pseudo-class": false,
+        "focus-visible-pseudo-class": false,
+        "focus-within-pseudo-class": false,
+        "color-functional-notation": false,
       },
     }),
   ],
