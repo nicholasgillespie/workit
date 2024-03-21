2fb546d6ec52c05262d6f9b211467b27411b115a
Nicholas Gillespie <contact@nicholasgillespie.com>
2024-03-21 20:29:55 +0100

    Update styles and configurations

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,11 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-
 import vitePurgeCss from "vite-plugin-purgecss";
-
-console.log("__dirname: ", __dirname);
 
 export default defineConfig({
   plugins: [vitePurgeCss()],
   base: "/workit",
-  // root: resolve(__dirname, "/"),
-  // build: {
-  //   outDir: "../dist",
-  //   emptyOutDir: true,
-  //   rollupOptions: {
-  //     input: {
-  //       home: resolve(__dirname, "/index.html"),
-  //     },
-  //   },
-  // },
   build: {
-    // outDir: "/dist",
     emptyOutDir: true,
     rollupOptions: {
       input: {
